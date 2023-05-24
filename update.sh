cc79a763c26b3ecbc1082add70ac806ed1e43308
Lukman Priya <134487594+LawNetwork@users.noreply.github.com>
2023-05-24 16:19:26 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt update
+apt upgrade -y
+
+clear
+echo -e "
+=======================================================
+|                AutoScript by -Lawnet-               |
+|          Script Version: 0.1 Beta May 2023          |
+======================================================="
+sleep 3
+read -ne "Do you want to update the Autoscript? (y/n): " -e update
+
+if [  ]
