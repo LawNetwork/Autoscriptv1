8b45ef7c508630eb28be3f1835c34d1470b2fdfd
Lukman Priya <134487594+LawNetwork@users.noreply.github.com>
2023-06-01 17:44:57 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
+source /root/color
 
+scversion=$(curl -sS https://raw.githubusercontent.com/LawNetwork/Autoscript/main/version);
+scvpsversion=$(cat /root/version);
+
+if [ "$scversion" = "$scvpsversion" ]; then
 clear
-echo -e "
-=======================================================
-|                AutoScript by -Lawnet-               |
-|          Script Version: 0.1 Beta May 2023          |
-======================================================="
-sleep 3
-read -ne "Do you want to update the Autoscript? (y/n): " -e update
+echo -e "${green}There is no update available yet"; sleep 1
+echo -e "${green}Feel free to give us any suggestions on what other features can we add into"; skeep 1
+echo ""
+echo -e "${yellow}Contact us at https://t.me/Law_sky"
+echo ""
+echo ""; sleep 5
+echo -ne "${none}Press enter to go back" read bck
+if [ -z "$bck" ]; then
+    clear
+fi
 
-if [  ]
+fi
+menu
